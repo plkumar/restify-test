bf65159a02c55cfb95401dff7520a4f0c87b1406
Lakshman Peethani <lpeethani@allianceglobalservices.com>
2016-05-06 12:48:01 +0530

    few minor changes.

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ server.use(restify.authorizationParser());
 server.use(restify.dateParser());
 server.use(restify.queryParser());
 server.use(restify.urlEncodedBodyParser());
-server.use(function slowHandler(req, res, next) {
-    setTimeout(function () {
-        return next();
-    }, 250);
-});
+// server.use(function slowHandler(req, res, next) {
+//   setTimeout(function () {
+//     return next();
+//   }, 250);
+// });
 server.get({ path: '/hello/:name', name: 'GetFoo' }, function respond(req, res, next) {
     res.send({
         hello: req.params.name
@@ -24,7 +24,7 @@ server.get(/\/static\/?.*/, restify.serveStatic({
     directory: __dirname + '/public',
     default: 'index.html'
 }));
-server.listen(8080, function () {
+server.listen(process.env.PORT || 8080, function () {
     console.log('%s listening at %s', server.name, server.url);
 });
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@
 
 import restify = require('restify');
 
-var server:restify.Server = restify.createServer({
+var server: restify.Server = restify.createServer({
   name: 'restify-test'
 });
 
@@ -12,13 +12,13 @@ server.use(restify.dateParser());
 server.use(restify.queryParser());
 server.use(restify.urlEncodedBodyParser());
 
-server.use(function slowHandler(req, res, next) {
-  setTimeout(function() {
-    return next();
-  }, 250);
-});
+// server.use(function slowHandler(req, res, next) {
+//   setTimeout(function () {
+//     return next();
+//   }, 250);
+// });
 
-server.get({path: '/hello/:name', name: 'GetFoo'}, function respond(req, res, next) {
+server.get({ path: '/hello/:name', name: 'GetFoo' }, function respond(req, res, next) {
   res.send({
     hello: req.params.name
   });
@@ -30,6 +30,7 @@ server.get(/\/static\/?.*/, restify.serveStatic({
   default: 'index.html'
 }));
 
-server.listen(8080, function () {
-    console.log('%s listening at %s', server.name, server.url);
+server.listen(process.env.PORT || 8080, function () {
+  console.log('%s listening at %s', server.name, server.url);
 });
+
